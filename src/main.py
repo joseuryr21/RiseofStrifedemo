d9091a49197aeec59f76be676bcf3c95478d6313
joseuryr21 <joseuryr21@gmail.com>
2022-04-25 16:44:49 -0400

    Basic game loop
    Basic game loop
    
    added loop counter
    
    Closes #51

---
+++ src/main.py
@@ -0,0 +1,24 @@
+#the game loop
+
+import pygame
+from view.renderGame import renderGame
+
+screen = pygame.display.set_mode([800, 800])
+isRunning = True
+cntLoopnum = 1
+
+def checkQuit():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
+            return isRunning
+    isRunning = True
+    return isRunning
+
+
+
+while isRunning:
+    pygame.time.delay(20)
+    isRunning = checkQuit()
+    renderGame(screen)
+    cntLoopnum += 1
--- src/view/renderGame.py
+++ src/view/renderGame.py
@@ -5,3 +5,4 @@ from model.gamedata import takeHit
 def renderGame():
     print("rendergame")
     pygame.display.flip()
+    screen.fill((255, 255, 255)) #white
