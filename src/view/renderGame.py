f2e147a9e4743e1b8f1c347f2a3f462fbacafccb
joseuryr21 <joseuryr21@gmail.com>
2022-04-26 16:08:33 -0400

    draw menu frame
    Close #4

--- src/view/renderGame.py
+++ src/view/renderGame.py
@@ -16,11 +16,19 @@ def renderGround(screen, currentDirectory):
     groundSprite = pygame.image.load(currentDirectory + '/images/terrain/Ground1.png')
     screen.blit(groundSprite, (0, 384)) #384 is below sky
 
+def renderMenuFrame(screen, currentDirectory):
+    menuFrameSprite = pygame.image.load(currentDirectory + '/images/menu/menuframe/MenuFrame1.png')
+    screen.blit(menuFrameSprite, (0, 792)) #792 is below ground
+
+def renderMenus(screen, currentDirectory):
+    renderMenuFrame(screen, currentDirectory)
+
 
 def renderGame(screen):
     currentDirectory = getDirectory()
     screen.fill((255, 255, 255)) #white
     renderSky(screen, currentDirectory)
     renderGround(screen, currentDirectory)
+    renderMenus(screen, currentDirectory)
     pygame.display.flip()
     screen.fill((255, 255, 255)) #white
