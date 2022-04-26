caf99655a288060f7d0305d2f1c9191fc15b2f6b
joseuryr21 <joseuryr21@gmail.com>
2022-04-26 15:24:42 -0400

    draw sky
    Close #1

--- src/view/renderGame.py
+++ src/view/renderGame.py
@@ -1,8 +1,21 @@
 #draws images and such
 import pygame
+import os
 from model.gamedata import takeHit
 
-def renderGame():
-    print("rendergame")
+def getDirectory():
+    currentDirectory = str(os.getcwd())
+    currentDirectory = currentDirectory.replace("\\",'/')
+    return currentDirectory
+
+def renderSky(screen, currentDirectory):
+    skySprite = pygame.image.load(currentDirectory + '/images/terrain/Sky1.png')
+    screen.blit(skySprite, (0, 0))
+
+
+def renderGame(screen):
+    currentDirectory = getDirectory()
+    screen.fill((255, 255, 255)) #white
+    renderSky(screen, currentDirectory)
     pygame.display.flip()
     screen.fill((255, 255, 255)) #white
