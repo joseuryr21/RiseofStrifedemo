a816ac17e305e4ec60811713adddec610cb55395
joseuryr21 <joseuryr21@gmail.com>
2022-04-26 15:56:54 -0400

    draw ground
    Close #2

--- src/view/renderGame.py
+++ src/view/renderGame.py
@@ -12,10 +12,15 @@ def renderSky(screen, currentDirectory):
     skySprite = pygame.image.load(currentDirectory + '/images/terrain/Sky1.png')
     screen.blit(skySprite, (0, 0))
 
+def renderGround(screen, currentDirectory):
+    groundSprite = pygame.image.load(currentDirectory + '/images/terrain/Ground1.png')
+    screen.blit(groundSprite, (0, 384)) #384 is below sky
+
 
 def renderGame(screen):
     currentDirectory = getDirectory()
     screen.fill((255, 255, 255)) #white
     renderSky(screen, currentDirectory)
+    renderGround(screen, currentDirectory)
     pygame.display.flip()
     screen.fill((255, 255, 255)) #white
