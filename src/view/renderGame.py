a02f0f60ec3e7654ef43ddc2b3f21ebbac4e3fc1
joseuryr21 <joseuryr21@gmail.com>
2022-04-28 18:25:02 -0400

    added character stats
    Close #11

--- src/view/renderGame.py
+++ src/view/renderGame.py
@@ -293,7 +293,86 @@ def renderMenus(screen, currentDirectory):
     renderButtons(screen, currentDirectory)
     renderPortaits(screen, currentDirectory)
     renderBars(screen, currentDirectory)
+statsCharacter = {
+  "friendly1": [
+    {"health": 100},
+    {"mana": 50},
+    {"stamina": 3},
+    {"attack": 20},
+    {"magic": 20},
+    {"defense": 30},
+    {"resistance": 20},
+    {"buff1": 'none'},
+    {"buff2": 'none'},
+    {"debuff1": 'none'},
+    {"debuff2": 'none'}
+  ],
+  "friendly2": [
+    {"health": 100},
+    {"mana": 50},
+    {"stamina": 3},
+    {"attack": 20},
+    {"magic": 20},
+    {"defense": 30},
+    {"resistance": 20},
+    {"buff1": 'none'},
+    {"buff2": 'none'},
+    {"debuff1": 'none'},
+    {"debuff2": 'none'}
+  ],
+  "friendly3": [
+    {"health": 50},
+    {"mana": 20},
+    {"stamina": 2},
+    {"attack": 20},
+    {"magic": 20},
+    {"defense": 30},
+    {"resistance": 20},
+    {"buff1": 'none'},
+    {"buff2": 'none'},
+    {"debuff1": 'none'},
+    {"debuff2": 'none'}
+  ],
 
+  "enemy1": [
+    {"health": 100},
+    {"mana": 50},
+    {"stamina": 3},
+    {"attack": 20},
+    {"magic": 20},
+    {"defense": 30},
+    {"resistance": 20},
+    {"buff1": 'none'},
+    {"buff2": 'none'},
+    {"debuff1": 'none'},
+    {"debuff2": 'none'}
+  ],
+  "enemy2": [
+    {"health": 100},
+    {"mana": 50},
+    {"stamina": 3},
+    {"attack": 20},
+    {"magic": 20},
+    {"defense": 30},
+    {"resistance": 20},
+    {"buff1": 'none'},
+    {"buff2": 'none'},
+    {"debuff1": 'none'},
+    {"debuff2": 'none'}
+  ],
+  "enemy3": [
+    {"health": 2},
+    {"mana": 78},
+    {"stamina": 2},
+    {"attack": 20},
+    {"magic": 20},
+    {"defense": 30},
+    {"resistance": 20},
+    {"buff1": 'none'},
+    {"buff2": 'none'},
+    {"debuff1": 'none'},
+    {"debuff2": 'none'}
+  ]
 
 def renderGame(screen):
     currentDirectory = getDirectory()
@@ -302,3 +381,4 @@ def renderGame(screen):
     renderGround(screen, currentDirectory)
     renderMenus(screen, currentDirectory)
     pygame.display.flip()
+}
