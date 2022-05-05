84edfa02c2b3b286c7fb4b1ffdf247dee222c57f
joseuryr21 <joseuryr21@gmail.com>
2022-05-04 20:43:42 -0400

    comments for quick indexes & readded comma

--- src/model/gamedata.py
+++ src/model/gamedata.py
@@ -12,7 +12,7 @@ statsCharacter = {
     {"buff1": 'none'},
     {"buff2": 'none'},
     {"debuff1": 'none'},
-    {"debuff2": 'none'}
+    {"debuff2": 'none'},
     {"action": 'none'}
   ],
   "friendly2": [
@@ -26,7 +26,7 @@ statsCharacter = {
     {"buff1": 'none'},
     {"buff2": 'none'},
     {"debuff1": 'none'},
-    {"debuff2": 'none'}
+    {"debuff2": 'none'},
     {"action": 'none'}
   ],
   "friendly3": [
@@ -40,7 +40,7 @@ statsCharacter = {
     {"buff1": 'none'},
     {"buff2": 'none'},
     {"debuff1": 'none'},
-    {"debuff2": 'none'}
+    {"debuff2": 'none'},
     {"action": 'none'}
   ],
 
@@ -55,7 +55,7 @@ statsCharacter = {
     {"buff1": 'none'},
     {"buff2": 'none'},
     {"debuff1": 'none'},
-    {"debuff2": 'none'}
+    {"debuff2": 'none'},
     {"action": 'none'}
   ],
   "enemy2": [
@@ -69,21 +69,21 @@ statsCharacter = {
     {"buff1": 'none'},
     {"buff2": 'none'},
     {"debuff1": 'none'},
-    {"debuff2": 'none'}
+    {"debuff2": 'none'},
     {"action": 'none'}
   ],
   "enemy3": [
-    {"health": 100},
-    {"mana": 50},
-    {"stamina": 3},
-    {"attack": 20},
-    {"magic": 20},
-    {"defense": 30},
-    {"resistance": 20},
-    {"buff1": 'none'},
-    {"buff2": 'none'},
-    {"debuff1": 'none'},
-    {"debuff2": 'none'}
+    {"health": 100}, #0
+    {"mana": 50}, #1
+    {"stamina": 3}, #2
+    {"attack": 20}, #3
+    {"magic": 20}, #4
+    {"defense": 30}, #5
+    {"resistance": 20}, #6
+    {"buff1": 'none'}, #7
+    {"buff2": 'none'}, #8
+    {"debuff1": 'none'}, #9
+    {"debuff2": 'none'}, #10
     {"action": 'none'} #11
   ]
 
