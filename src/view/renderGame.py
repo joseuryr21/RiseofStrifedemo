93f9d90bee7c49b1b53eec8925ccc381b7204037
joseuryr21 <joseuryr21@gmail.com>
2022-05-10 20:29:30 -0400

    made renderTiles shorter
    Close #63

--- src/view/renderGame.py
+++ src/view/renderGame.py
@@ -5,6 +5,7 @@ from model.gamedata import statsCharacter
 
 tileSprites = ["/images/terrain/tileNormal.png", "/images/terrain/tileSelected.png", "/images/terrain/tileAttacked.png", "/images/terrain/tileHealed.png", "/images/terrain/tileLingered.png"]
 tilePositions = [[80, 464], [80, 668], [264, 412], [264, 564], [264, 716], [448, 412], [448, 564], [448, 716], [632, 412], [632, 564], [632, 716], [816, 464], [816, 668], [1044, 464], [1044, 668], [1228, 412], [1228, 564], [1228, 716], [1412, 412], [1412, 564], [1412, 716], [1596, 412], [1596, 564], [1596, 716], [1780, 464], [1780, 668]]
+tileHoverPositions = [[20, 392, 196, 588], [20, 592, 196, 788], [204, 392, 380, 508], [204, 516, 380, 660], [204, 668, 380, 788], [388, 392, 564, 508], [388, 516, 564, 660], [388, 668, 564, 788], [572, 392, 740, 508], [572, 516, 740, 660], [572, 668, 740, 788], [756, 392, 932, 588], [756, 592, 932, 788], [984, 392, 1160, 588], [984, 592, 1160, 788], [1168, 392, 1344, 508], [1168, 516, 1344, 660], [1168, 668, 1344, 788], [1352, 392, 1528, 508], [1352, 516, 1528, 660], [1352, 668, 1528, 788], [1536, 392, 1712, 508], [1536, 516, 1712, 660], [1536, 668, 1712, 788], [1720, 392, 1896, 588], [1720, 592, 1896, 788]]
 effectPositions = [[[1268, 812], [1268, 848], [1308, 848]], [[1268, 900], [1268, 936], [1308, 936]], [[1268, 988], [1268, 1024], [1308, 1024]], [[1580, 812], [1580, 848], [1540, 848]], [[1580, 900], [1580, 936], [1540, 936]], [[1580, 988], [1580, 1024], [1540, 1024]]]
 
 def getDirectory():
@@ -86,62 +87,9 @@ def checkTileStatus(mousePosition, tileSprites, topLeftX, topLeftY, bottomRightX
 
 def renderTiles(screen, currentDirectory):
     mousePosition = pygame.mouse.get_pos()
-    #friendlyside
-    tile1 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 20, 392, 196, 588))
-    tile2 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 20, 592, 196, 788))
-    tile3 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 204, 392, 380, 508))
-    tile4 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 204, 516, 380, 660))
-    tile5 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 204, 668, 380, 788))
-    tile6 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 388, 392, 564, 508))
-    tile7 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 388, 516, 564, 660))
-    tile8 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 388, 668, 564, 788))
-    tile9 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 572, 392, 740, 508))
-    tile10 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 572, 516, 740, 660))
-    tile11 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 572, 668, 740, 788))
-    tile12 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 756, 392, 932, 588))
-    tile13 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 756, 592, 932, 788))
-    #enemyside
-    tile14 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 984, 392, 1160, 588))
-    tile15 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 984, 592, 1160, 788))
-    tile16 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1168, 392, 1344, 508))
-    tile17 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1168, 516, 1344, 660))
-    tile18 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1168, 668, 1344, 788))
-    tile19 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1352, 392, 1528, 508))
-    tile20 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1352, 516, 1528, 660))
-    tile21 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1352, 668, 1528, 788))
-    tile22 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1536, 392, 1712, 508))
-    tile23 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1536, 516, 1712, 660))
-    tile24 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1536, 668, 1712, 788))
-    tile25 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1720, 392, 1896, 588))
-    tile26 = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, 1720, 592, 1896, 788))
-    #friendlyside
-    screen.blit(tile1, (80, 464))
-    screen.blit(tile2, (80, 668))
-    screen.blit(tile3, (264, 412))
-    screen.blit(tile4, (264, 564))
-    screen.blit(tile5, (264, 716))
-    screen.blit(tile6, (448, 412))
-    screen.blit(tile7, (448, 564))
-    screen.blit(tile8, (448, 716))
-    screen.blit(tile9, (632, 412))
-    screen.blit(tile10, (632, 564))
-    screen.blit(tile11, (632, 716))
-    screen.blit(tile12, (816, 464))
-    screen.blit(tile13, (816, 668))
-    #enemyside
-    screen.blit(tile14, (1044, 464))
-    screen.blit(tile15, (1044, 668))
-    screen.blit(tile16, (1228, 412))
-    screen.blit(tile17, (1228, 564))
-    screen.blit(tile18, (1228, 716))
-    screen.blit(tile19, (1412, 412))
-    screen.blit(tile20, (1412, 564))
-    screen.blit(tile21, (1412, 716))
-    screen.blit(tile22, (1596, 412))
-    screen.blit(tile23, (1596, 564))
-    screen.blit(tile24, (1596, 716))
-    screen.blit(tile25, (1780, 464))
-    screen.blit(tile26, (1780, 668))
+    for i in range(0, 26):
+        tile = pygame.image.load(currentDirectory + checkTileStatus(mousePosition, tileSprites, tileHoverPositions[i][0], tileHoverPositions[i][1], tileHoverPositions[i][2], tileHoverPositions[i][3]))
+        screen.blit(tile, (tilePositions[i][0], tilePositions[i][1]))
 
 def renderPortaits(screen, currentDirectory):
     friendly1PortraitSprite = pygame.image.load(currentDirectory + '/images/menu/blueMenu/friendlyPortrait/portrait1.png')
