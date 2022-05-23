3e3cde1561e79bc46a9463be3eea6165153698d3
joseuryr21 <joseuryr21@gmail.com>
2022-05-23 15:20:18 -0400

    reduced variable lifespan & fixed indentation

--- src/view/renderGame.py
+++ src/view/renderGame.py
@@ -28,17 +28,18 @@ def renderMenuFrame(screen, currentDirectory):
     screen.blit(menuFrameSprite, (0, 792)) #792 is below ground
 
 def renderButtons(screen, currentDirectory):
-    attackSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Attack1.png')
-    defendSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Defend1.png')
-    specialSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Special1.png')
-    relocateSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Relocate2.png')
+    if currentMenu() == 'main':
+        attackSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Attack1.png')
+        screen.blit(attackSprite, (28, 824))
+        defendSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Defend1.png')
+        screen.blit(defendSprite, (428, 824))
+        specialSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Special1.png')
+        screen.blit(specialSprite, (28, 948))
+        relocateSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Relocate2.png')
+        screen.blit(relocateSprite, (428, 948))
     backSprite = pygame.image.load(currentDirectory + '/images/menu/grayMenu/mainbuttons/Back1.png')
-    exitSprite = pygame.image.load(currentDirectory + '/images/menu/Exit1.png')
-    screen.blit(attackSprite, (28, 824))
-    screen.blit(defendSprite, (428, 824))
-    screen.blit(specialSprite, (28, 948))
-    screen.blit(relocateSprite, (428, 948))
     screen.blit(backSprite, (828, 824))
+    exitSprite = pygame.image.load(currentDirectory + '/images/menu/Exit1.png')
     screen.blit(exitSprite, (1840, 0))
 
 def createBarSpritesheet(currentDirectory, index, characterType, statType):
